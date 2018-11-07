5c5707602348c2e256b03f2630c02c80d6ba5c7b
Jayne bracken Sanford <jsanford@e3r6p3.21-school.ru>
2018-11-07 18:08:53 +0300

    NORME

--- bsq_v5/ft_finder.c
+++ bsq_v5/ft_finder.c
@@ -6,50 +6,35 @@
 /*   By: kkihn <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 19:39:46 by kkihn             #+#    #+#             */
-/*   Updated: 2018/11/07 15:53:57 by kkihn            ###   ########.fr       */
+/*   Updated: 2018/11/07 18:08:35 by jsanford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-void 	ft_finder(int **arr, int len, int width, char *str)
+int g_i = 0;
+int g_j = 0;
+
+void	ft_finder(int **arr, int len, int width, char *str)
 {
 	int j;
 	int i;
-	int min;
-	int I;
-	int J;
-	
-	min = 0;
-	I = 0;
-	J = 0;
+
 	i = len - 1;
 	while (i >= 0)
-	{       
+	{
 		j = width - 1;
 		while (j >= 0)
-		{	if (arr[i][j] != 0)
-			{
-				min = arr[i][j + 1];
-				if (arr[i + 1][j] < min)
-					min = arr[i + 1][j];
-				if (arr[i + 1][j + 1] < min)
-					min = arr[i + 1][j + 1];
-				arr[i][j] = min + arr[i][j];
-            	if (arr[i][j] >= arr[I][J])
-            	{
-            		I = i;
-                	J = j;
-            	}
-            }
+		{
+			ft_extra(i, j, arr);
 			j--;
 		}
 		i--;
 	}
-	print_solution(str, arr[I][J], I, J);
+	print_solution(str, arr[g_i][g_j]);
 }
 
-void    print_solution(char *str, int size, int I, int  J)
+void	print_solution(char *str, int size)
 {
 	int		i;
 	int		j;
@@ -58,14 +43,12 @@ void    print_solution(char *str, int size, int I, int  J)
 	i = 0;
 	j = 0;
 	full = return_params(str, 'f');
-	while (*str != '\n')
+	while (*(str - 1) != '\n')
 		str++;
-	str++;
 	while (*str != '\0')
 	{
-
-		if (i >= I && i <= I + size - 1 && j >= J && j <= J + size - 1)
-		{	
+		if (i >= g_i && i <= g_i + size - 1 && j >= g_j && j <= g_j + size - 1)
+		{
 			write(1, &full, 1);
 		}
 		else
@@ -77,5 +60,25 @@ void    print_solution(char *str, int size, int I, int  J)
 		}
 		str++;
 		j++;
+	}
+}
+
+void	ft_extra(int i, int j, int **arr)
+{
+	int min;
+
+	if (arr[i][j] != 0)
+	{
+		min = arr[i][j + 1];
+		if (arr[i + 1][j] < min)
+			min = arr[i + 1][j];
+		if (arr[i + 1][j + 1] < min)
+			min = arr[i + 1][j + 1];
+		arr[i][j] = min + arr[i][j];
+		if (arr[i][j] >= arr[g_i][g_j])
+		{
+			g_i = i;
+			g_j = j;
+		}
 	}
 }
